69685e544cb05f935af280f589c7a0f8ae448f9c
“marcusngwj” <marcusngwj@msn.com>
2017-01-21 23:14:03 +0800

    Add message handler to echo messages sent to box in cmd prompt **For testing purposes**

--- kboxLyricsBot.py
+++ kboxLyricsBot.py
@@ -1,4 +1,6 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from bs4 import BeautifulSoup
+import requests
 
 def start(bot, update):
     update.message.reply_text(
@@ -12,11 +14,23 @@ def start(bot, update):
 def hello(bot, update):
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
+		
+def getSongTitle(bot, update):
+	print(update.message.text)
 
 updater = Updater('329555371:AAG6VH8AiLUJJCF5Vp1tGcJ6TFymho-_ArU')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, getSongTitle))
+
+page = "https://genius.com/"
+requestResult = requests.get(page)
+
+requestHtmlContent = requestResult.content
+
+soup = BeautifulSoup(requestHtmlContent, "html.parser")
 
 updater.start_polling()
 updater.idle()
