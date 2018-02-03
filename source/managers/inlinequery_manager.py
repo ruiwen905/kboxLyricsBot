f1733068182f131bace296e82f6d7ad15e9eae09
marcusngwj <marcusngwj@msn.com>
2018-02-03 21:49:44 +0800

    add get lyrics method

--- source/managers/inlinequery_manager.py
+++ source/managers/inlinequery_manager.py
@@ -5,7 +5,6 @@ from reference.reference_manager import ReferenceManager
 
 # Ref: https://python-telegram-bot.readthedocs.io/en/stable/telegram.inlinequery.html
 class InlineQueryManager:
-
     def __init__(self, bot, update):
         self.bot = bot
         self.inline_query = update.inline_query
@@ -15,9 +14,7 @@ class InlineQueryManager:
         result_song_list = ReferenceManager.get_song_list(song_to_search)
         result_display_list = []
         i = 0
-        print(result_song_list)
         for song in result_song_list:
-            print(song.title)
-            result_display_list.append(InlineQueryResultArticle(id=i, title=song.title, input_message_content=InputTextMessageContent(song.url), url=song.url, description=song.artist, thumb_url=song.thumbnail_url, thumb_height=640, thumb_width=640))
+            result_display_list.append(InlineQueryResultArticle(id=song.url, title=song.title, input_message_content=InputTextMessageContent(song.lyrics), url=song.url, hide_url=True, description=song.artist, thumb_url=song.thumbnail_url, thumb_height=640, thumb_width=640))
             i += 1
         self.inline_query.answer(results=result_display_list)
